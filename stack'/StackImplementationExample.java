e31e7a6ad04ef39f7394e0e34e4a9273b05a1020
Hari om patidar <patidarh930@gmail.com>
2026-07-06 06:23:49 +0000

    incomplete stack implementation

---
+++ stack'/StackImplementationExample.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class StackImplementationExample {
+    static int stack[];
+    static int top;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of stack");
+        int size = sc.nextInt();
+        System.out.println("Stack size is: " + size);
+
+        while (true) {
+            System.out.println("Choose any one Option");
+            System.out.println("1.push\n2.pop\n3.peek\n4.size\n5.isEmpty\n6.isFull\n7.display\n8.search\n9.clear\n10.exit");
+            int opt = sc.nextInt();
+            switch (opt) {
+                case 1:
+
+                    break;
+
+                case 2:
+
+                    break;
+
+                case 3:
+
+                    break;
+
+                case 4:
+
+                    break;
+
+                case 5:
+
+                    break;
+
+                case 6:
+
+                    break;
+                case 7:
+
+                    break;
+                case 8:
+                    break;
+
+                case 9:
+                    break;
+                    
+                case 10:
+                    break;
+            }
+        }
+
+    }
+
+}
